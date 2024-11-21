93b3bb43e6c8268ecf44089c16450cb13291c390
Yuxian Wu <12212614@mail.sustech.edu.cn>
2024-11-22 06:58:38 +0800

    单机版

--- assignment/assignment2/2024FallCS209A-A2Demo/src/main/java/org/example/demo/Game.java
+++ assignment/assignment2/2024FallCS209A-A2Demo/src/main/java/org/example/demo/Game.java
@@ -1,5 +1,9 @@
 package org.example.demo;
 
+import javafx.scene.control.Button;
+
+import java.util.Random;
+
 public class Game {
 
     // row length
@@ -11,62 +15,93 @@ public class Game {
     // board content
     int[][] board;
 
-    public Game(int[][] board){
+    Button[] path;
+
+    public Game(int[][] board) {
         this.board = board;
         this.row = board.length;
         this.col = board[0].length;
     }
 
     // randomly initialize the game board
-    public static int[][] SetupBoard(int row, int col) {
+    public static int[][] SetupBoard(int row, int col, boolean reset) {
+        Random random = new Random();
+
+        if (!reset){
+            row += 2;
+            col += 2;
+        }
 
         // TODO: randomly initialize board
+        int[][] board = new int[row][col];
 
-        return new int[][]{
-                {0, 0, 0, 0, 0, 0},
-                {0, 1, 1, 2, 2, 0},
-                {0, 3, 3, 4, 4, 0},
-                {0, 5, 5, 6, 6, 0},
-                {0, 7, 7, 8, 8, 0},
-                {0, 0, 0, 0, 0, 0}
-        };
+        int totalPairs = (row - 2) * (col - 2) / 2;
+        int[] icons = new int[totalPairs * 2];
+        for (int i = 0; i < totalPairs; i++) {
+            icons[i * 2] = random.nextInt(1,12);
+            icons[i * 2 + 1] = icons[i * 2];
+        }
+
+        for (int i = 0; i < icons.length; i++) {
+            int randomIndex = random.nextInt(icons.length);
+            int temp = icons[i];
+            icons[i] = icons[randomIndex];
+            icons[randomIndex] = temp;
+        }
+
+        // Fill board with shuffled icons
+        int index = 0;
+        for (int i = 1; i < row - 1; i++) {
+            for (int j = 1; j < col - 1; j++) {
+                board[i][j] = icons[index++];
+            }
+        }
+
+        return board;
     }
 
     // judge the validity of an operation
-    public boolean judge(int row1, int col1, int row2, int col2){
+    public boolean judge(int row1, int col1, int row2, int col2, Button[][] buttons) {
         if ((board[row1][col1] != board[row2][col2]) || (row1 == row2 && col1 == col2)) {
             return false;
         }
 
         // one line
         if (isDirectlyConnected(row1, col1, row2, col2, board)) {
+            path = new Button[]{buttons[row1][col1], buttons[row2][col2]};
             return true;
         }
 
         // two lines
-        if((row1 != row2) && (col1 != col2)){
-            if(board[row1][col2] == 0 && isDirectlyConnected(row1, col1, row1, col2, board)
-            && isDirectlyConnected(row1, col2, row2, col2, board))
+        if ((row1 != row2) && (col1 != col2)) {
+            if (board[row1][col2] == 0 && isDirectlyConnected(row1, col1, row1, col2, board)
+                    && isDirectlyConnected(row1, col2, row2, col2, board)) {
+                path = new Button[]{buttons[row1][col1], buttons[row1][col2], buttons[row2][col2]};
                 return true;
-            if(board[row2][col1] == 0 && isDirectlyConnected(row2, col2, row2, col1, board)
-            && isDirectlyConnected(row2, col1, row1, col1, board))
+            }
+            if (board[row2][col1] == 0 && isDirectlyConnected(row2, col2, row2, col1, board)
+                    && isDirectlyConnected(row2, col1, row1, col1, board)) {
+                path = new Button[]{buttons[row1][col1], buttons[row2][col1], buttons[row2][col2]};
                 return true;
+            }
         }
 
         // three lines
-        if(row1 != row2)
+        if (row1 != row2)
             for (int i = 0; i < board[0].length; i++) {
                 if (board[row1][i] == 0 && board[row2][i] == 0 &&
                         isDirectlyConnected(row1, col1, row1, i, board) && isDirectlyConnected(row1, i, row2, i, board)
-                        && isDirectlyConnected(row2, col2, row2, i, board)){
+                        && isDirectlyConnected(row2, col2, row2, i, board)) {
+                    path = new Button[]{buttons[row1][col1], buttons[row1][i], buttons[row2][i], buttons[row2][col2]};
                     return true;
                 }
             }
-        if(col1 != col2)
-            for (int j = 0; j < board.length; j++){
+        if (col1 != col2)
+            for (int j = 0; j < board.length; j++) {
                 if (board[j][col1] == 0 && board[j][col2] == 0 &&
                         isDirectlyConnected(row1, col1, j, col1, board) && isDirectlyConnected(j, col1, j, col2, board)
-                        && isDirectlyConnected(row2, col2, j, col2, board)){
+                        && isDirectlyConnected(row2, col2, j, col2, board)) {
+                    path = new Button[]{buttons[row1][col1], buttons[j][col1], buttons[j][col2], buttons[row2][col2]};
                     return true;
                 }
             }

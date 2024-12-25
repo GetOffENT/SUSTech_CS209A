7386dbc25b5a4f5705925ea45bba23cdab010ce8
Yuxian Wu <12212614@mail.sustech.edu.cn>
2024-12-26 03:58:46 +0800

    remove useless resources

--- assignment/assignment2/resources/materialfx_demo_win64/MFXDemo_Win/bin/MaterialFX Demo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DIR="%~dp0"
-set JAVA_EXEC="%DIR:"=%\java"
-
-
-
-pushd %DIR% & %JAVA_EXEC% %CDS_JVM_OPTS% -Dglass.disableGrab=true -p "%~dp0/../app" -m MaterialFX.Demo/io.github.palexdev.materialfx.demo.Demo  %* & popd
